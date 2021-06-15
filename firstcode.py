8712856c5d673043bedb73ec9e720b47b559d8dd
kamal hasan <hasan.kamaal0072@gmail.com>
2021-06-14 23:10:54 -0800

    Python Basics

---
+++ firstcode.py
@@ -0,0 +1,21 @@
+first_name="john"
+last_name="khan"
+roll=105
+a=10
+b=a
+a=12
+print(id(a))
+print(id(a))
+print(id(b))
+list1=['kama',12,'khan',23.15]
+print("The For Loop Is going to apply From here....")
+for x in list1:
+    print(x)
+    if x==12:
+        break
+    else:
+        print("This is outside of For Loop")
+print("End Of for Loop")
+print(type(list1))
+print(type(first_name))
+print(type(roll))
